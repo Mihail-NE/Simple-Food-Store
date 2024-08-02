a5067b5e671e350b745e7c7ff0f9920d5bf8eec9
Mihail <harkunovmihail953@gmail.com>
2024-08-02 07:35:22 +0300

    added input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Button from "./components/Button/Button";
+import Input from "./components/Input/Input";
 
 function App() {
     return (
         <>
-            <Button>Кнопка</Button>
+            <Button appearence="small">Кнопка</Button>
+            <Button appearence="big">Кнопка</Button>
+            <Input placeholder="Email"/>
         </>
     );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,12 +2,20 @@ import styles from "./Button.module.css";
 import { ButtonProps } from "./Button.props";
 import cn from "classnames";
 
-const Button = ({ children, className, ...props }: ButtonProps) => {
+const Button = ({
+    children,
+    className,
+    appearence = "small",
+    ...props
+}: ButtonProps) => {
     return (
         <div>
             <button
                 {...props}
-                className={cn(styles["button"], styles["accent"], className)}
+                className={cn(styles["button"], styles["accent"], className, {
+                    [styles["small"]]: appearence === "small",
+                    [styles["big"]]: appearence === "big",
+                })}
             >
                 {children}
             </button>
