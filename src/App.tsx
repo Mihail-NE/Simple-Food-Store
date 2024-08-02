9002ea3ccc8332701cd25edf5b06b38dcebc8d44
Mihail <harkunovmihail953@gmail.com>
2024-08-02 15:09:58 +0300

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { Routes, Route } from "react-router-dom";
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
+import Menu from "./Pages/Menu/Menu";
+import Cart from "./Pages/Cart/Cart";
+import Error from "./Pages/Error/Error";
 
 function App() {
     return (
@@ -7,8 +11,18 @@ function App() {
             <Button appearence="small">Кнопка</Button>
             <Button appearence="big">Кнопка</Button>
             <Input placeholder="Email"/>
+            <div>
+                <a href="/">Menu</a>
+                <a href="/cart">Cart</a>
+            </div>
+
+            <Routes>
+                <Route path="/" element={<Menu />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
         </>
     );
 }
 
-export default App;
+export default App; 
