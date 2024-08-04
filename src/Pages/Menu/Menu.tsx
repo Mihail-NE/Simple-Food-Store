cf57976e9429b42803b08eb638e12bd09afdae41
Mihail <harkunovmihail953@gmail.com>
2024-08-04 22:16:31 +0300

    styles for card

--- src/Pages/Menu/Menu.tsx
+++ src/Pages/Menu/Menu.tsx
@@ -1,14 +1,42 @@
-import styles from './Menu.module.css';
-import Search from '../../components/Search/Search';
-import Heading from './../../components/Heading/Heading';
+import styles from "./Menu.module.css";
+import Search from "../../components/Search/Search";
+import Heading from "./../../components/Heading/Heading";
+import ProductCard from "../../components/ProductCarD/ProductCard";
 
 const Menu = () => {
     return (
-        <div className={styles["head"]}>
-            <Heading>Меню</Heading>
-            <Search placeholder='Введите название блюда' />
-
-        </div>
+        <>
+            <div className={styles["head"]}>
+                <Heading>Меню</Heading>
+                <Search placeholder="Введите название блюда" />
+            </div>
+            <div>
+                <ProductCard
+                    id={1}
+                    title="Маргарита"
+                    discription="Томаты, сыр моцарелла,оливковое масло, пряный базилик"
+                    rating={4.5}
+                    price={100}
+                    image="./public/margarita.png"
+                />
+                <ProductCard
+                    id={1}
+                    title="Маргарита"
+                    discription="Томаты, сыр моцарелла,оливковое масло, пряный базилик"
+                    rating={4.5}
+                    price={100}
+                    image="./public/margarita.png"
+                />
+                <ProductCard
+                    id={1}
+                    title="Маргарита"
+                    discription="Томаты, сыр моцарелла,оливковое масло, пряный базилик"
+                    rating={4.5}
+                    price={100}
+                    image="./public/margarita.png"
+                />
+            </div>
+        </>
     );
 };
 
--- src/components/ProductCarD/ProductCard.tsx
+++ src/components/ProductCarD/ProductCard.tsx
@@ -1,32 +1,32 @@
-import styles from './ProductCard.module.css'
-import { ProductCardProps } from './ProductCard.props'
+import styles from "./ProductCard.module.css";
+import { ProductCardProps } from "./ProductCard.props";
 
 const ProductCard = (props: ProductCardProps) => {
-  return (
-    <div className={styles["card"]}>
-        <div className={styles["head"]}>
-            <div className={styles["price"]}>
-                {props.price}
+    return (
+        <div className={styles["card"]}>
+            <div
+                className={styles["head"]}
+                style={{ backgroundImage: `url(${props.image})` }}
+            >
+                <div className={styles["price"]}>
+                    {props.price}
+                    <span className={styles["currency"]}> ₽</span>
+                </div>
+                <button className={styles["add-to-cart"]}>
+                    <img src="./public/add-to-card.svg" />
+                </button>
+                <div className={styles["range"]}>
+                    {props.rating}
+                    <img src="./public/star.svg" />
+                </div>
             </div>
-            <button className={styles["add-to-cart"]}>
-              <img src="./public/add-to-card.svg" />
-            </button>
-            <div className={styles["range"]}>
-                {props.rating}
-                <img src="./public/star.svg" />
+
+            <div className={styles["footer"]}>
+                <div className={styles["title"]}>{props.title}</div>
+                <div className={styles["description"]}>{props.discription}</div>
             </div>
         </div>
+    );
+};
 
-        <div className={styles["footer"]}>
-            <div className={styles["title"]}>
-                {props.title}
-            </div>
-            <div className={styles["discription"]}>
-                {props.discription}
-            </div>
-        </div>
-    </div>
-  )
-}
-
-export default ProductCard
+export default ProductCard;
