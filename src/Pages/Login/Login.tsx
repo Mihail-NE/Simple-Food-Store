a3cbb1cd0eeb0a6a0f5a420aa054a5f75de42793
Mihail <harkunovmihail953@gmail.com>
2024-08-08 22:53:43 +0300

    fix login

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -4,31 +4,59 @@ import Heading from "../../components/Heading/Heading";
 import Input from "../../components/Input/Input";
 import styles from "./Login.module.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { PREFIX } from "../../Help/API";
+
+
+export interface LoginForm {
+    email: {
+        value: string;
+    };
+    password: {
+        value: string;
+    };
+}
 
 const Login = () => {
     const sumbit = (e: FormEvent) => {
-      e.preventDefault()
-      console.log(e);
-    }
+        e.preventDefault();
+        const target = e.target as typeof e.target & LoginForm;
+        const { email, password } = target;
+        console.log(email.value);
+        console.log(password.value);
+        sendLogin(email.value, password.value);
+    };
 
-
+    const sendLogin = async (email: string, password: string) => {
+        const { data } = await axios.post(`${PREFIX}/auth/login`, {
+            email,
+            password,
+        });
+        console.log(data)
+    };
 
     return (
-        <div onSubmit={sumbit}>
+        <div>
             <Heading>Вход</Heading>
-            <form className={styles["form"]}>
+            <form className={styles["form"]} onSubmit={sumbit}>
                 <div className={styles["email"]}>
                     <label className={styles["label"]} htmlFor="email">
                         Ваш email
                     </label>
-                    <Input placeholder="Email" id="email" />
+                    <Input placeholder="Email" id="email" name="email" />
                 </div>
 
                 <div className={styles["password"]}>
                     <label className={styles["label"]} htmlFor="password">
                         Ваш пароль
                     </label>
-                    <Input id="password" placeholder="Пароль" type="password" />
+                    <Input
+                        id="password"
+                        placeholder="Пароль"
+                        type="password"
+                        name="password"
+                    />
+
                 </div>
 
                 <div className={styles["links"]}>
